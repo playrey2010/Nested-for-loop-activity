23cfdd8b295a067ba45fb6707562639ef589d39a
playrey2010 <playrey2010@gmail.com>
2019-09-08 22:14:22 -0400

    finished

---
+++ src/OneOneTwo.java
@@ -0,0 +1,11 @@
+public class OneOneTwo {
+    public static void main(String[] args) {
+        for( int i = 0; i < 10; i++) {
+            for (int x = 0; x< i; x++ ){
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+    }
+
+}
